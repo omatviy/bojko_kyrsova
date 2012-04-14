5801e28387b86c64a1e8eb844f8f22543ed9588d
Sasha Matviy <omatviy@gmail.com>
2012-04-14 17:26:02 +0300

    Firtst initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttt::Application.config.secret_token = '19b3199d9dc4aa59432f3ba6a175da6a68c5a69f22fde1be046b8c3f31900263947293882bdbe6dea3a0dce7a54578b194528486fbf9c8070a8ae0e806552078'
