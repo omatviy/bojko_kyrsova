c02c3480164a79a0f3abcdf410f67b618a7ea827
Sasha Matviy <omatviy@gmail.com>
2012-04-14 18:43:23 +0300

    Changed migration file create_language

--- db/migrate/20120414144237_create_languages.rb
+++ db/migrate/20120414144237_create_languages.rb
@@ -1,8 +1,15 @@
 class CreateLanguages < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :languages do |t|
-
+      t.string :iana_code, :limit => 10, :default => nil
+      t.string :name, :null => false
       t.timestamps
     end
+
+    add_index :languages, :name
+  end
+
+  def self.down
+    drop_table :languages
   end
 end
