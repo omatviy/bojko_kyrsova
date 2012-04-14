4d93a13e4359c3888d5750c57b02978b6e9329dc
Sasha Matviy <omatviy@gmail.com>
2012-04-14 20:03:49 +0300

    Add to seed loading of language

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Loading language data"
+Language.load_csv_data('languages.csv',false,';')
